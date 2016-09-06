381d829a6acb2975acf1449d26648f05bb94bd2e
algyon <algyon@gmail.com>
2016-09-06 17:34:04 +0200

    Ajout de la page primer + core .js updated

--- JS/core.js
+++ JS/core.js
@@ -19,5 +19,17 @@ $( document ).ready(function() {
     $("#managment").click(function(){
         $( "#content" ).load( "Genes_management.php" );
     });
+
+
+    /** Compteur de nucléotides **/
+    String.prototype.count=function(s1) {
+    return (this.length - this.replace(new RegExp(s1,"gi"), '').length) / s1.length;
+    }
+    var primer1 = $("#primerA").val()
+    nbA = primer1.count('a')
+    nbT = primer1.count('t')
+    nbG = primer1.count('g')
+    nbC = primer1.count('c')
 });
+
 
