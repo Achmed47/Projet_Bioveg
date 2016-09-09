135153fd24843c7406bb2f5d74476a74e45ece10
algyon <algyon@gmail.com>
2016-09-09 16:49:26 +0200

    Blast functionnal

--- JS/blast.js
+++ JS/blast.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var Database_blast = "";
+
     $("#btn_number_accession").click(function(){
         $("#list_gene").hide();
         $("#num_access").show();
@@ -9,26 +11,26 @@ $(document).ready(function() {
         $("#list_gene").show();
     });
 
-    $("li.liste_dropdown").click(function(){
-        $("dropdownMenu1").val($(this).text()+"<span class=\"caret\"></span>");
+    $("li.liste_dropdown_bd").click(function(){
+        $("#dropdownMenu_bd_blast").html($(this).text()+"<span class=\"caret\"></span>");
+        Database_blast = $(this).data("numaccession");
     });
 
     $("li.liste_dropdown_blast").click(function(){
-        $("dropdownMenu2").html($(this).text()+"<span class=\"caret\"></span>");
+        $("#dropdownMenu_blast").html($(this).text()+"<span class=\"caret\"></span>");
     });
 
     $("#start_blast").click(function(){
         if ($("#num_access").css("display") != "none"){
-            var N_Accession = $("#special_access_number").text();
+            var N_Accession = $("#special_access_number").val();
         }
         else {
-            var N_Accession = $("btn_liste_gene").text();
+            var N_Accession = $("#btn_liste_gene").text();
         };
-        var Database_blast = $("").text() ;
-        var Type_blast = $("").text();
-        var request_html = "http://blast.ncbi.nlm.nih.gov/Blast.cgi?QUERY="+N_Accession+"&DATABASE="+Database_blast+"&PROGRAM="+Type_blast+"&FILTER=L&EXPECT=0.01&FORMAT_TYPE=HTML&NCBI_GI=on&HITLIST_SIZE=10&CMD=Put";
+        var Type_blast = $("#dropdownMenu_blast").text();
+        var request_html = "http://blast.ncbi.nlm.nih.gov/Blast.cgi?QUERY="+N_Accession+"&DATABASE=nr&EQ_MENU="+Database_blast+"&EQ_OP=AND&PROGRAM="+Type_blast+"&FILTER=L&EXPECT=0.01&FORMAT_TYPE=HTML&NCBI_GI=on&HITLIST_SIZE=10&CMD=Put";
 
-        console.log(N_Accession + "+" + Database_blast +"+"+ Type_blast);
+        console.log(request_html)
 
         window.open(request_html,"_blank");
     });
